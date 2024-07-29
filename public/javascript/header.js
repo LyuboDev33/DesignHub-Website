6467030f74623128c5db011978015d1c5f9601bb
LyuboDev33 <166536413+LyuboDev33@users.noreply.github.com>
2024-07-29 09:49:35 +0300

    Add files via upload

---
+++ public/javascript/header.js
@@ -0,0 +1,67 @@
+let product_numbers = document.querySelectorAll('.cart_products_number');
+let numbArray = JSON.parse(sessionStorage.getItem('data'));
+
+if(numbArray === null || numbArray.length === 0) {
+  product_numbers.forEach(function(product_numb) {
+    product_numb.innerHTML = '';
+  });
+} else {
+  product_numbers.forEach(function(product_numb) {
+    product_numb.innerHTML = numbArray.length;
+  });
+}
+
+
+
+let sidebarBackground = document.querySelector('.cart_sidebar_container')
+let insideSidebar = document.querySelector('.cart_sidebar_modal')
+
+// let menuBar = document.querySelector('.menu_bar');
+
+// menuBar.addEventListener('click', function () {
+// sidebarBackground.style.display = 'flex';
+// setTimeout(function () {
+//   insideSidebar.style.transition = 'transform 0.5s ease';
+//   insideSidebar.style.transform = "translateX(16%)";
+// }, 100);
+// });
+
+// let closeSidebarBtn = document.querySelector('.closeSidebar');
+
+
+// closeSidebarBtn.addEventListener('click', function () {
+
+// insideSidebar.style.transition = 'transform 0.5s ease';
+// insideSidebar.style.transform = "translateX(120%)";
+
+// setTimeout(function () {
+//   sidebarBackground.style.display = 'none';
+
+// }, 500)
+
+// });
+
+// function updateResize () { 
+
+//   if(window.innerWidth > 768) { 
+//     sidebarBackground.style.display = 'none';
+//     insideSidebar.style.transform = "translateX(120%)";
+//   }
+
+// }
+
+// updateResize ();
+
+// window.addEventListener("resize", updateResize);
+
+
+setTimeout(function () { 
+  let cookieModal = document.querySelector('.js-cookie-consent');
+
+  if(cookieModal) { 
+
+    cookieModal.style.transform = 'translateY(-40px)';
+    cookieModal.style.transition = "2s";
+  }
+
+}, 5000)
